fce8c5c053fe9b3f7df7582e43bd479a964bea82
Rifqi Pratama <yosuakurniawan2322@gmail.com>
2023-06-22 19:15:30 +0700

    add liked article

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 
 export default function ArticleCard({ article }) {
+  const [liked, setLiked] = useState(false); 
+
+  const isLoggedIn = localStorage.getItem('token') !== null;
+
+  useEffect(() => {
+    const fetchLikeStatus = async () => {
+      try {
+        const token = localStorage.getItem('token');
+
+        const headers = {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        };
+
+        const response = await axios.get('https://minpro-blog.purwadhikabootcamp.com/api/blog/pagLike', { headers });
+
+        const likedArticles = response.data.result; 
+
+        const isLiked = likedArticles.some((likedArticle) => likedArticle.BlogId === article.id);
+
+        setLiked(isLiked); 
+      } catch (error) {
+        console.error('Error fetching liked articles', error);
+      }
+    };
+
+    fetchLikeStatus();
+  }, [article.id]);
+
+
+  const handleLike = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      };
+
+      await axios.post('https://minpro-blog.purwadhikabootcamp.com/api/blog/like', { BlogId: article.id }, { headers });
+
+      setLiked(true);
+    } catch (error) {
+      console.error('Error liking the article', error);
+    }
+  };
+
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
       <img
         className="rounded-t-lg h-52 w-full object-cover"
         src={`https://minpro-blog.purwadhikabootcamp.com/${article.imageURL}`}
         alt={article.title}
       />
 
-      <div className="flex gap-2 p-4">
+      <div className="flex flex-wrap gap-2 p-4">
         {article.Blog_Keywords.map((blog) => (
           <div className="border border-gray-500 px-3 py-1 rounded-full" key={blog.id}>
             {blog.Keyword.name}
@@ -25,6 +74,28 @@ export default function ArticleCard({ article }) {
         <small className="text-zinc-600">{article.createdAt}</small>
 
         <p className="my-3 font-normal text-gray-500 dark:text-gray-400">{article.content}</p>
+
+      </div>
+
+      <div className='mt-auto px-4 py-4'>
+        {isLoggedIn && (
+          <button
+          className="flex items-center gap-1 text-gray-500 hover:text-gray-700 focus:outline-none"
+          onClick={handleLike}
+        >
+          {liked ? (
+            <>
+              <AiFillLike className="h-5 w-5 text-red-500" />
+              <span>Liked</span>
+            </>
+          ) : (
+            <>
+              <AiOutlineLike className="h-5 w-5" />
+              <span>Like</span>
+            </>
+          )}
+        </button>
+        )}
       </div>
     </div>
   );
